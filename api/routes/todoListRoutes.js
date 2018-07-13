4826be5faa6ad762b334facd84b4718087a1d338
= <hiputraa@gmail.com>
2018-07-13 22:51:17 +0700

    Change routes again

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -3,11 +3,11 @@ module.exports = function(app) {
 	var todoList = require('../controllers/todoListController');
 
 	//todoList Routes
-	app.route('api/tasks')
+	app.route('/api/tasks')
 		.get(todoList.list_all_tasks)
 		.post(todoList.create_a_task);
 
-	app.route('api/tasks/:taskId')
+	app.route('/api/tasks/:taskId')
 		.get(todoList.read_a_task)
 		.put(todoList.update_a_task)
 		.delete(todoList.delete_a_task);
